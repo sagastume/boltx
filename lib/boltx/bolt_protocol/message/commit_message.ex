04c217e6a34687c48c6c445aa863ae4482146bee
Luis Sagastume <8935075+sagastume@users.noreply.github.com>
2023-12-13 08:08:04 -0600

    feat: implementation of messages for creating explicit transactions

---
+++ lib/boltx/bolt_protocol/message/commit_message.ex
@@ -0,0 +1,26 @@
+defmodule Boltx.BoltProtocol.Message.CommitMessage do
+
+  alias Boltx.Internals.PackStream.Message.Encoder
+  alias Boltx.Internals.PackStream.Message.Decoder
+
+  def encode(bolt_version) when is_float(bolt_version) and bolt_version >= 3.0 do
+    message = []
+    Encoder.do_encode(:commit, message, 3)
+  end
+
+  def encode(_, _) do
+    {:error, %Boltx.Internals.Error{code: :unsupported_message_version, message: "COMMIT message version not supported"}}
+  end
+
+  @spec decode(float(), <<_::16, _::_*8>>) :: {:error, Boltx.Error.t()} | {:ok, any()}
+  def decode(_bolt_version, binary_messages) do
+    messages = Enum.map(binary_messages, &Decoder.decode(&1, 1))
+
+    case hd(messages) do
+      {:success, response} ->
+        {:ok, response}
+      {:failure, response} ->
+        {:error, Boltx.Error.wrap(__MODULE__, %{code: response["code"], message: response["message"]})}
+    end
+  end
+end
