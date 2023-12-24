9582ea8a5c2a0a2d3681313458810c13085fba08
Luis Sagastume <8935075+sagastume@users.noreply.github.com>
2023-12-24 08:39:53 -0600

    ci(test): Add more tests to execute queries

--- test/boltx_test.exs
+++ test/boltx_test.exs
@@ -211,6 +211,197 @@ defmodule BoltxTest do
 
       assert {2, 1} == {length(path.nodes), length(path.relationships)}
     end
+
+    @tag :core
+    test "return a single number from a statement with params", c do
+      row = Boltx.query!(c.conn, "RETURN $n AS num", %{n: 10}) |> Response.first()
+      assert row["num"] == 10
+    end
+
+    @tag :core
+    test "run simple statement with complex params", c do
+      row =
+        Boltx.query!(c.conn, "RETURN $x AS n", %{x: %{abc: ["d", "e", "f"]}})
+        |> Response.first()
+
+      assert row["n"]["abc"] == ["d", "e", "f"]
+    end
+
+    @tag :core
+    test "return an array of numbers", c do
+      row = Boltx.query!(c.conn, "RETURN [10,11,21] AS arr") |> Response.first()
+      assert row["arr"] == [10, 11, 21]
+    end
+
+    @tag :core
+    test "return a string", c do
+      row = Boltx.query!(c.conn, "RETURN 'Hello' AS salute") |> Response.first()
+      assert row["salute"] == "Hello"
+    end
+
+    @tag :core
+    test "UNWIND range(1, 10) AS n RETURN n", c do
+      assert %Response{results: rows} = Boltx.query!(c.conn, "UNWIND range(1, 10) AS n RETURN n")
+      assert {1, 10} == rows |> Enum.map(& &1["n"]) |> Enum.min_max()
+    end
+
+    @tag :core
+    test "MERGE (k:Person {name:'Kote'}) RETURN k", c do
+      k =
+        Boltx.query!(c.conn, "MERGE (k:Person {name:'Kote', boltx: true}) RETURN k LIMIT 1")
+        |> Response.first()
+        |> Map.get("k")
+
+      assert k.labels == ["Person"]
+      assert k.properties["name"] == "Kote"
+    end
+
+    @tag :core
+    test "query/2 and query!/2", c do
+      assert r = Boltx.query!(c.conn, "RETURN [10,11,21] AS arr")
+      assert [10, 11, 21] = Response.first(r)["arr"]
+
+      assert {:ok, %Response{} = r} = Boltx.query(c.conn, "RETURN [10,11,21] AS arr")
+      assert [10, 11, 21] = Response.first(r)["arr"]
+    end
+
+    @tag :core
+    test "create a Bob node and check it was deleted afterwards", c do
+      assert %Response{stats: stats, plan: plan} =
+               Boltx.query!(c.conn, "CREATE (a:Person {name:'Bob'})")
+
+      assert stats["labels-added"] == 1
+      assert stats["nodes-created"] == 1
+      assert stats["properties-set"] == 1
+
+      assert ["Bob"] ==
+               Boltx.query!(c.conn, "MATCH (a:Person {name: 'Bob'}) RETURN a.name AS name")
+               |> Enum.map(& &1["name"])
+
+      assert %Response{stats: stats} =
+               Boltx.query!(c.conn, "MATCH (a:Person {name:'Bob'}) DELETE a")
+
+      assert stats["nodes-deleted"] == 1
+    end
+
+    @tag :core
+    test "can execute a query after a failure", c do
+      assert {:error, _} = Boltx.query(c.conn, "INVALID CYPHER")
+      assert {:ok, %Response{results: [%{"n" => 22}]}} = Boltx.query(c.conn, "RETURN 22 as n")
+    end
+
+    @tag :core
+    test "negative numbers are returned as negative numbers", c do
+      assert {:ok, %Response{results: [%{"n" => -1}]}} = Boltx.query(c.conn, "RETURN -1 as n")
+    end
+
+    @tag :core
+    test "return a simple node", c do
+      assert %Response{
+               results: [
+                 %{
+                   "p" => %Boltx.Types.Node{
+                     id: _,
+                     labels: ["Person"],
+                     properties: %{"boltx" => true, "name" => "Patrick Rothfuss"}
+                   }
+                 }
+               ]
+             } = Boltx.query!(c.conn, "MATCH (p:Person {name: 'Patrick Rothfuss'}) RETURN p")
+    end
+
+    @tag :core
+    test "Simple relationship", c do
+      cypher = """
+        MATCH (p:Person)-[r:WROTE]->(b:Book {title: 'The Name of the Wind'})
+        RETURN r
+      """
+
+      assert %Response{
+               results: [
+                 %{
+                   "r" => %Boltx.Types.Relationship{
+                     end: _,
+                     id: _,
+                     properties: %{},
+                     start: _,
+                     type: "WROTE"
+                   }
+                 }
+               ]
+             } = Boltx.query!(c.conn, cypher)
+    end
+
+    @tag :core
+    test "simple path", c do
+      cypher = """
+      MERGE p = ({name:'Alice', boltx: true})-[:KNOWS]->({name:'Bob', boltx: true})
+      RETURN p
+      """
+
+      assert %Response{
+               results: [
+                 %{
+                   "p" => %Boltx.Types.Path{
+                     nodes: [
+                       %Boltx.Types.Node{
+                         id: _,
+                         labels: [],
+                         properties: %{"boltx" => true, "name" => "Alice"}
+                       },
+                       %Boltx.Types.Node{
+                         id: _,
+                         labels: [],
+                         properties: %{"boltx" => true, "name" => "Bob"}
+                       }
+                     ],
+                     relationships: [
+                       %Boltx.Types.UnboundRelationship{
+                         id: _,
+                         properties: %{},
+                         type: "KNOWS"
+                       }
+                     ],
+                     sequence: [1, 1]
+                   }
+                 }
+               ]
+             } = Boltx.query!(c.conn, cypher)
+    end
+
+    @tag :bolt_3_x
+    @tag :bolt_4_x
+    @tag :bolt_5_x
+    test "Cypher with plan resul", c do
+      assert %Response{plan: plan} = Boltx.query!(c.conn, "EXPLAIN RETURN 1")
+      refute plan == nil
+      assert Regex.match?(~r/[3|4|5]/iu, plan["args"]["planner-version"])
+    end
+
+    @tag :bolt_3_x
+    @tag :bolt_4_x
+    @tag :bolt_5_x
+    test "EXPLAIN MATCH (n), (m) RETURN n, m", c do
+      assert %Response{notifications: notifications, plan: plan} =
+               Boltx.query!(c.conn, "EXPLAIN MATCH (n), (m) RETURN n, m")
+
+      refute notifications == nil
+      refute plan == nil
+
+      if Regex.match?(~r/CYPHER 3/iu, plan["args"]["planner-version"]) do
+        assert "CartesianProduct" ==
+                 plan["children"]
+                 |> List.first()
+                 |> Map.get("operatorType")
+      else
+        assert(
+          "CartesianProduct@neo4j" ==
+            plan["children"]
+            |> List.first()
+            |> Map.get("operatorType")
+        )
+      end
+    end
   end
 
   defp connect(c) do
